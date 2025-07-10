5e6c8bbe75c82902cd1a72df3ee724fc285504c5
Brandt Ryan <emailme@brandtryan.com>
2025-07-09 20:27:45 -0400

    Rstream hiccup-canvas Animation

--- examples/noc2-example-000/src/index.ts
+++ examples/noc2-example-000/src/index.ts
@@ -1,9 +1,91 @@
 // SPDX-License-Identifier: Apache-2.0
-import { canvas2d } from "@thi.ng/canvas";
-import { analogRgb } from "@thi.ng/color";
-import { asRGB, NUM_THEMES } from "@thi.ng/color-palettes";
+import { adaptiveCanvas2d, canvas2d } from "@thi.ng/canvas";
+import {
+	colorFromRange,
+	CSS_LEVEL4,
+	lch,
+	setDefaultCSSConversions,
+} from "@thi.ng/color";
+import { arc, closestPoint, group, withAttribs } from "@thi.ng/geom";
 import { draw } from "@thi.ng/hiccup-canvas";
-import { rad } from "@thi.ng/math";
-import { pickRandom, SYSTEM } from "@thi.ng/random";
-import { map, range2d } from "@thi.ng/transducers";
-import { madd2 } from "@thi.ng/vectors";
+import { fit01, TAU } from "@thi.ng/math";
+import { SYSTEM, weightedRandom } from "@thi.ng/random";
+import { fromDOMEvent, fromRAF } from "@thi.ng/rstream";
+import { map, normRange } from "@thi.ng/transducers";
+import { dist2, type Vec } from "@thi.ng/vectors";
+
+// optional, configure CSS color conversions to use use CSS Color Level 4
+// (i.e. direct support for LCH colors and avoiding conversions to sRGB)
+setDefaultCSSConversions(CSS_LEVEL4);
+
+const W = Math.min(window.innerWidth, window.innerHeight);
+const ORIGIN = [W / 2, W / 2];
+
+const PICK_DIST = 6;
+const PICK_COL = "cyan";
+
+const { canvas, ctx } = canvas2d(W, W, document.getElementById("app"));
+
+// custom stroke weight distribution
+const strokeWeight = weightedRandom(
+	// thickness
+	[1, 2, 4, 8, 16, 32],
+	// weight
+	[4, 8, 4, 2, 1, 0.25]
+);
+
+// generate random arc configurations
+const arcs = [
+	...map(
+		(i) => ({
+			// radius
+			r: fit01(i, 50, W * 0.45),
+			// stroke width
+			w: strokeWeight(),
+			// randomized color
+			col: colorFromRange("warm", {
+				base: lch(0.8, 0.8, 0),
+				variance: 0.125,
+			}),
+			// start angle
+			theta: SYSTEM.float(TAU),
+			// angle spread
+			spread: SYSTEM.float(TAU),
+			// rotation speed
+			speed: SYSTEM.norm(0.03),
+		}),
+		normRange(50)
+	),
+];
+
+const mouse = fromDOMEvent(canvas, "mousemove").transform(
+	map((e): Vec => {
+		const b = canvas.getBoundingClientRect();
+		return [e.clientX - b.left, e.clientY - b.top];
+	})
+);
+
+fromRAF().subscribe({
+	next() {
+		arcs.forEach((a) => (a.theta += a.speed));
+		const m = mouse.deref();
+		draw(
+			ctx,
+			group(
+				{ __background: "#222" },
+				arcs.map(({ r, w, col, theta, spread }) => {
+					const a = arc(ORIGIN, r, 0, theta, theta + spread);
+					return withAttribs(a, {
+						weight: w,
+						stroke:
+							m &&
+								dist2(m, closestPoint(a, m)!) <
+								Math.max(PICK_DIST, w)
+								? PICK_COL
+								: col,
+					});
+				})
+			)
+		);
+	},
+});

3f051b7adac6b684e859dc9cd2cec9511a3ef4cb
Brandt Ryan <emailme@brandtryan.com>
2025-07-12 08:04:16 -0400

    Fix 015

--- examples/noc2-example-015/src/index.ts
+++ examples/noc2-example-015/src/index.ts
@@ -1,63 +1,54 @@
-import { type Vec, add, sub, normalize, limit, ZERO2 } from "@thi.ng/vectors";
+import { type Vec, mulN, sub, mag } from "@thi.ng/vectors";
 import { fromRAF } from "@thi.ng/rstream";
 import { canvas2d } from "@thi.ng/canvas";
 import { draw } from "@thi.ng/hiccup-canvas";
-import { circle, line } from "@thi.ng/geom";
+import { line } from "@thi.ng/geom";
 
-// SETUP
-const WIDTH = 640;
-const HEIGHT = 240;
-const { ctx } = canvas2d(WIDTH, HEIGHT, document.getElementById("app"));
-
-interface Mover {
-	pos: Vec;
-	vel: Vec;
-}
-
+// --- SETUP ---
+const canvasSize: Vec = [window.innerWidth, window.innerHeight];
+const { ctx } = canvas2d(canvasSize[0], canvasSize[1], document.getElementById("app"));
+const CENTER: Vec = [canvasSize[0] / 2, canvasSize[1] / 2];
+const ORIGIN: Vec = [0, 0];
+// --- STATE ---
 interface AppState {
-	mover: Mover;
+	mousePos: Vec;
 }
 
 let state: AppState = {
-	mover: {
-		pos: [100, 100],
-		vel: [2.5, 2]
-	},
+	mousePos: CENTER,
 };
 
-// UPDATE
-const update = (currentState: AppState): AppState => {
-	const mover = { ...currentState.mover };
-	mover.pos = add(null, mover.pos, mover.vel);
+// --- INPUT (Event Handling) ---
+document.addEventListener("mousemove", (event) => {
+	state.mousePos = [event.clientX, event.clientY];
+});
 
-	// Edges
-	if (mover.pos[0] > WIDTH || mover.pos[0] < 0) {
-		mover.vel[0] = mover.vel[0] * -1;
-	}
-	if (mover.pos[1] > HEIGHT || mover.pos[1] < 0) {
-		mover.vel[1] = mover.vel[1] * -1;
-	}
+// --- VIEW (Drawing Logic) ---
+const view = (currentState: AppState) => {
+	// Perform vector subtraction/multiplication
+	// Return an array of shapes (the scene description)
+	return [
+		// Draw a white background to clear the canvas
+		["rect", { fill: "#fff", stroke: "rgb(0, 0, 0)" }, [0, 0],
+			canvasSize[0], canvasSize[1]],
 
-	return {
-		...currentState,
-		mover,
-	};
+		// Draw multiplied vector from a transformed ORIGIN to mouseposition
+		["g",
+			{ stroke: "rgb(0, 0, 0)", weight: 24 },
+			line([0, 0], [mag(currentState.mousePos), 0]),
+		],
+
+		// Draw line from center to mouseposition
+		["g", { stroke: "rgb(127,127,127)", weight: 2, translate: CENTER },
+			line(ORIGIN, currentState.mousePos),
+		],
+	];
 };
 
-// VIEW
-const view = (currentState: AppState) => [
-	"g", {},
-	["rect", { fill: "#333" }, [0, 0], WIDTH, HEIGHT],
-	["g", { fill: "none", stroke: "#fff", "stroke-width": 2 },
-		circle(currentState.mover.pos, 24)
-	]
-];
-
+// --- MAIN LOOP --- //
 fromRAF().subscribe({
 	next() {
-		const newState = update(state);
-		const scene = view(newState);
+		const scene = view(state);
 		draw(ctx, scene);
-		state = newState;
 	}
 });

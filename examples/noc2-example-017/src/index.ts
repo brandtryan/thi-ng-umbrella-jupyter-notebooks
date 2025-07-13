d09243efe0aa4589e08e615eb7506278cc207fe1
Brandt Ryan <emailme@brandtryan.com>
2025-07-13 11:24:50 -0400

    Complete 017

--- examples/noc2-example-017/src/index.ts
+++ examples/noc2-example-017/src/index.ts
@@ -1,63 +1,35 @@
-import { type Vec, add, sub, normalize, limit, ZERO2 } from "@thi.ng/vectors";
+import { circle, group, translate } from "@thi.ng/geom";
+import { $canvas } from "@thi.ng/rdom-canvas";
 import { fromRAF } from "@thi.ng/rstream";
-import { canvas2d } from "@thi.ng/canvas";
-import { draw } from "@thi.ng/hiccup-canvas";
-import { circle, line } from "@thi.ng/geom";
+import { mulN, normalize, add, sub, type Vec } from "@thi.ng/vectors";
 
-// SETUP
-const WIDTH = 640;
-const HEIGHT = 240;
-const { ctx } = canvas2d(WIDTH, HEIGHT, document.getElementById("app"));
+const WIDTH = 600;
+const HEIGHT = 600;
+const RADIUS = 24;
 
-interface Mover {
-	pos: Vec;
-	vel: Vec;
-}
+let pos: Vec = [300, 300];
+let vel: Vec = [2, 2.5];
 
-interface AppState {
-	mover: Mover;
-}
+// create geometry stream/subscription
+const geo = fromRAF().map(() => {
+	add(pos, pos, vel);
 
-let state: AppState = {
-	mover: {
-		pos: [100, 100],
-		vel: [2.5, 2]
-	},
-};
-
-// UPDATE
-const update = (currentState: AppState): AppState => {
-	const mover = { ...currentState.mover };
-	mover.pos = add(null, mover.pos, mover.vel);
-
-	// Edges
-	if (mover.pos[0] > WIDTH || mover.pos[0] < 0) {
-		mover.vel[0] = mover.vel[0] * -1;
+	if (pos[0] < RADIUS || pos[0] > WIDTH - RADIUS) {
+		vel[0] *= -1;
 	}
-	if (mover.pos[1] > HEIGHT || mover.pos[1] < 0) {
-		mover.vel[1] = mover.vel[1] * -1;
+	if (pos[1] < RADIUS || pos[1] > HEIGHT - RADIUS) {
+		vel[1] *= -1;
 	}
 
-	return {
-		...currentState,
-		mover,
-	};
-};
-
-// VIEW
-const view = (currentState: AppState) => [
-	"g", {},
-	["rect", { fill: "#333" }, [0, 0], WIDTH, HEIGHT],
-	["g", { fill: "none", stroke: "#fff", "stroke-width": 2 },
-		circle(currentState.mover.pos, 24)
-	]
-];
-
-fromRAF().subscribe({
-	next() {
-		const newState = update(state);
-		const scene = view(newState);
-		draw(ctx, scene);
-		state = newState;
-	}
+	return group(
+		{ __background: "#0ff" },
+		[
+			group({ stroke: "#000" }, [
+				circle(pos, RADIUS)
+			]),
+		]
+	);
 });
+
+// create & mount canvas component
+$canvas(geo, [WIDTH, HEIGHT]).mount(document.getElementById("app")!);

13af17b4b004645f368e5547804518dbcfdf8f60
Brandt Ryan <emailme@brandtryan.com>
2025-07-14 02:51:10 -0400

    Complete 110

--- examples/noc2-example-1-07/src/index.ts
+++ examples/noc2-example-1-07/src/index.ts
@@ -14,12 +14,24 @@ let vel: Vec = [2, 2.5];
 const geo = fromRAF().map(() => {
 	add(pos, pos, vel);
 
-	if (pos[0] < RADIUS || pos[0] > WIDTH - RADIUS) {
-		vel[0] *= -1;
+	if (pos[0] > WIDTH) {
+		pos[0] = 0;
+	} else if (pos[0] < 0) {
+		pos[0] = WIDTH;
 	}
-	if (pos[1] < RADIUS || pos[1] > HEIGHT - RADIUS) {
-		vel[1] *= -1;
+
+	if (pos[1] > HEIGHT) {
+		pos[1] = 0;
+	} else if (pos[1] < 0) {
+		pos[1] = HEIGHT;
 	}
+
+	// if (pos[0] < RADIUS || pos[0] > WIDTH - RADIUS) {
+	// 	vel[0] *= -1;
+	// }
+	// if (pos[1] < RADIUS || pos[1] > HEIGHT - RADIUS) {
+	// 	vel[1] *= -1;
+	// }
 
 	return group(
 		{ __background: "#0ff" },
